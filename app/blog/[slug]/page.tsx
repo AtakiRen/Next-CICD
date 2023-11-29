bc715c3df5d5bd7129bb36b9c3544ba76ec3f911
AtakiRen <hoangnam1772004@gmail.com>
2023-11-29 19:50:48 +0700

    update:

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,5 +9,8 @@ export async function generateMetadata({ params }: Params) {
 }
 
 export default function Page({ params }: Params) {
-  return <h1>Slug: {params.slug}<p>nam_test</p></h1>
+  return <h1>Slug: {params.slug}<p>nam_test</p>
+  <p>From FPT SOFTWARE</p>
+  <p>By AtakiSouma</p>
+  </h1>
 }
